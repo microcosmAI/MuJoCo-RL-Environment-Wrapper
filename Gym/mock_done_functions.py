98ac69a85e00b3631635a5e0a42290b47a8b61f4
doelfi <lisa.dollmann@web.de>
2023-04-21 18:33:03 +0200

    test: test_automated_testing passes all pytests for the private check-function in MuJoCo_RL class

---
+++ Gym/mock_done_functions.py
@@ -0,0 +1,11 @@
+def mock_doneFunction_correct():
+    return False, 5.
+
+def mock_doneFunction_boolean_4():
+    return 4, 5.
+
+def mock_doneFunction_boolean_string():
+    return "done", 5. 
+
+def mock_doneFunction_reward_int():
+    return True, 5
