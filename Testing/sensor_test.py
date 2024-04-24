9424cf51471e4d9dd515163587562a3c0bd2b17d
jonahschlie <jonah.schlie@gmail.com>
2024-04-24 16:49:51 +0200

    beautify code

--- Testing/sensor_test.py
+++ Testing/sensor_test.py
@@ -1,14 +1,16 @@
 import unittest
-from MuJoCo_Gym.mujoco_rl import *
+from MuJoCo_Gym.mujoco_rl import MuJoCoRL
 
-class TestSensorInObservationSpace(unittest.TestCase):
 
-    def test_touch_sensor(self):
-        '''
+class TestSensors(unittest.TestCase):
+    agents = ["receiver"]
+
+    def test_touch_sensor(self, agents=agents):
+        """
         Test case to verify that the touch sensor is included correctly in the observation space.
-        '''
+        """
         xml_file = "sensor_levels/Model1.xml"
-        agents = ["receiver"]
+
         config_dict = {"xmlPath": xml_file,
                        "agents": agents,
                        "rewardFunctions": [],
@@ -23,12 +25,11 @@ class TestSensorInObservationSpace(unittest.TestCase):
         self.assertEqual(env.observation_space.low, [0])
         self.assertEqual(env.observation_space.high, [20.0])
 
-    def test_accelerometer_sensor(self):
-        '''
+    def test_accelerometer_sensor(self, agents=agents):
+        """
         Test case to verify that the accelerometer sensor and all other sensors that create the same observation_space is included correctly.
-        '''
+        """
         xml_file = "sensor_levels/Model2.xml"
-        agents = ["receiver"]
         config_dict = {"xmlPath": xml_file,
                        "agents": agents,
                        "rewardFunctions": [],
@@ -43,12 +44,11 @@ class TestSensorInObservationSpace(unittest.TestCase):
         self.assertEqual(env.observation_space.low.tolist(), [-5.0, -5.0, -5.0])
         self.assertEqual(env.observation_space.high.tolist(), [5.0, 5.0, 5.0])
 
-    def test_rangefinder_sensor(self):
-        '''
+    def test_rangefinder_sensor(self, agents=agents):
+        """
         Test case to verify that the rangefinder sensor and all other sensors that create the same observation_space is included correctly.
-        '''
+        """
         xml_file = "sensor_levels/Model3.xml"
-        agents = ["receiver"]
         config_dict = {"xmlPath": xml_file,
                        "agents": agents,
                        "rewardFunctions": [],
@@ -63,13 +63,12 @@ class TestSensorInObservationSpace(unittest.TestCase):
         self.assertEqual(env.observation_space.low.tolist(), [-1.0])
         self.assertEqual(env.observation_space.high.tolist(), [10.0])
 
-    def test_frameaxis_sensors(self):
-        '''
+    def test_frameaxis_sensors(self, agents=agents):
+        """
         Test case to verify that the frame axis sensors are included correctly in the observation space.
-        '''
+        """
 
         xml_file = "sensor_levels/Model4.xml"
-        agents = ["receiver"]
         config_dict = {"xmlPath": xml_file,
                        "agents": agents,
                        "rewardFunctions": [],
@@ -83,6 +82,7 @@ class TestSensorInObservationSpace(unittest.TestCase):
         env = MuJoCoRL(config_dict=config_dict)
         self.assertEqual(env.observation_space.low.tolist(), [-1.0, -1.0, -1.0])
         self.assertEqual(env.observation_space.high.tolist(), [1.0, 1.0, 1.0])
+
 
 if __name__ == '__main__':
     unittest.main()
