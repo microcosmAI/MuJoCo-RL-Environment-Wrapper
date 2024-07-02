277b71b24cf3a73470cbbfc80d6fecdb483a00e7
jonahschlie <jonah.schlie@gmail.com>
2024-07-02 11:38:01 +0200

    benchmarking cpu and memory usage

--- benchmarking/fps_benchmark.py
+++ benchmarking/fps_benchmark.py
@@ -15,7 +15,7 @@ def get_config(xml_file, agents=None):
             "agents": agents,
             "rewardFunctions": [],
             "doneFunctions": [],
-            "skipFrames": 5,
+            "skipFrames": 0,
             "environmentDynamics": [],
             "freeJoint": True,
             "renderMode": False,
@@ -23,9 +23,7 @@ def get_config(xml_file, agents=None):
             "agentCameras": True}
 
 
-
-
-#Mulit Agent Environment Testing
+# Multi Agent Environment Testing
 env = MuJoCoRL(config_dict=get_config('levels/MultiAgentModel.xml', ['sender', 'receiver']))
 
 # List to store the FPS
@@ -63,10 +61,62 @@ for i in range(51):
     if i % 10 == 0:
         print("Episode {} done".format(i), "FPS: {}".format(frames_per_second[-1]))
 
+# Multi Agent Environment with two Sensors per agent - Testing
+env = MuJoCoRL(config_dict=get_config('levels/MultiAgentModel2Sensors.xml', ['sender', 'receiver']))
+
+# List to store the FPS
+frames_per_second_multi_2 = []
+
+# Running the environment for 50 episodes
+for i in range(51):
+    env.reset()
+    start = time.time()
+    # Running the environment for 1024 steps
+    for j in range(1024):
+        env.step({"sender": env.action_space('sender').sample(), "receiver": env.action_space('receiver').sample()})
+    end = time.time()
+    #
+    frames_per_second_multi_2.append(1024 / (end - start))
+    if i % 10 == 0:
+        print("Episode {} done".format(i), "FPS: {}".format(frames_per_second_multi_2[-1]))
+
+# Multi Agent Environment with three Sensors per agent - Testing
+env = MuJoCoRL(config_dict=get_config('levels/MultiAgentModel3Sensors.xml', ['sender', 'receiver']))
+
+# List to store the FPS
+frames_per_second_multi_3 = []
+
+# Running the environment for 50 episodes
+for i in range(51):
+    env.reset()
+    start = time.time()
+    # Running the environment for 1024 steps
+    for j in range(1024):
+        env.step({"sender": env.action_space('sender').sample(), "receiver": env.action_space('receiver').sample()})
+    end = time.time()
+    #
+    frames_per_second_multi_3.append(1024 / (end - start))
+    if i % 10 == 0:
+        print("Episode {} done".format(i), "FPS: {}".format(frames_per_second_multi_3[-1]))
+
 # Plotting the FPS
 plt.plot(frames_per_second, label="Single Agent")
 plt.plot(frames_per_second_multi, label="Multi Agent")
+# plt.plot(frames_per_second_multi_2, label="Multi Agent with 2 Sensors")
+# plt.plot(frames_per_second_multi_3, label="Multi Agent with 3 Sensors")
 plt.xlabel("Episode")
 plt.ylabel("FPS")
 plt.title("FPS per Episode")
+plt.legend()
+plt.show()
+
+# Plotting the FPS of Multi Agent Environments with different number of sensors
+# plt.plot(frames_per_second, label="Single Agent")
+plt.plot(frames_per_second_multi, label="Multi Agent")
+plt.plot(frames_per_second_multi_2, label="Multi Agent with 2 Sensors")
+plt.plot(frames_per_second_multi_3, label="Multi Agent with 3 Sensors")
+plt.xlabel("Episode")
+plt.ylabel("FPS")
+plt.title("FPS per Episode")
+plt.legend()
 plt.show()
